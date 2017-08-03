69320e088b0026dc2a8da05790e2179257d9835f
xunan623 <xunan623@outlook.com>
2017-08-03 11:27:01 +0800

    项目初始化

---
+++ XNWeChatSwift/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  XNWeChatSwift
+//
+//  Created by xunan on 2017/8/2.
+//  Copyright © 2017年 xunan. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        /** 注册云信 */
+        NIMSDK.shared().register(withAppID: kAppKey, cerName: nil)
+        
+        /** 初始化 */
+        
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ XNWeChatSwift/XNWeChatSwift-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <UIKit/UIKit.h>
+
+/** ================================= Pod =================================== */
+#import "AFNetworking.h"
+#import "SVProgressHUD.h"
+#import <NIMSDK/NIMSDK.h>
+//#import "NIMSDK.h"
+/** ================================= Lib =================================== */
+#import <CommonCrypto/CommonDigest.h>
+#import "TZImagePickerController.h"
+#import "KZVideoViewController.h"
+#import "KZVideoPlayer.h"
+#import "DACircularProgressView.h"
