5d486c49462b97fa256c5e6f535d7650544033a6
xunan623 <xunan623@outlook.com>
2017-08-04 15:16:11 +0800

    修改appdelegate

---
+++ XNWeChatSwift/Classses/Main/XNMainController.swift
@@ -0,0 +1,17 @@
+//
+//  XNMainController.swift
+//  XNWeChatSwift
+//
+//  Created by xunan on 2017/8/4.
+//  Copyright © 2017年 xunan. All rights reserved.
+//
+
+import UIKit
+
+class XNMainController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ XNWeChatSwift/Classses/User/XNUserController.swift
@@ -0,0 +1,17 @@
+//
+//  XNUserController.swift
+//  XNWeChatSwift
+//
+//  Created by xunan on 2017/8/4.
+//  Copyright © 2017年 xunan. All rights reserved.
+//
+
+import UIKit
+
+class XNUserController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
